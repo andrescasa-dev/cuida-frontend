c05ab876bf2e20f08a1f7fb70b81c4dfe3a8e5dc
Andres Castellanos <andres.about@gmail.com>
2024-05-13 20:43:15 -0500

    eslint and prettier were configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import React from "react";
-import { cn } from "@/lib/utils";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import React from 'react';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Cuida",
-  description: "Cuidado animal Cúcuteño",
+  title: 'Cuida',
+  description: 'Cuidado animal Cúcuteño',
 };
 
 export default function RootLayout({
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(inter.className,'grid grid-cols-main-layout')}>
+      <body className={cn(inter.className, 'grid grid-cols-main-layout')}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 export default function Home() {
-
-  return (
-    <div>First Commit</div>
-  );
+  const myConst = 123;
+  return <div>First Commit</div>;
 }
