e2ebf4153987129239ee1450e9500b7fc77d7c42
Andres Castellanos <andres.about@gmail.com>
2024-06-06 09:42:08 -0500

    mobile columns in list of pets

--- src/components/molecules/PetCard.tsx
+++ src/components/molecules/PetCard.tsx
@@ -20,7 +20,7 @@ function PetCard({
   return (
     <article
       className={cn(
-        'bg-muted flex flex-col justify-end rounded-[1.5rem] h-56 border border-border min-w-32 max-sm:max-w-44 sm:h-[17.25rem] lg:h-[22.5rem]',
+        'max-[320px]:max-w-[60vw] bg-muted flex flex-col justify-end rounded-[1.5rem] h-56 border border-border min-w-32 sm:h-[17.25rem] lg:h-[22.5rem]',
         className,
       )}
     >
@@ -34,29 +34,27 @@ function PetCard({
           alt={PhotoAlt || `Foto de un ${specie} ${size} y ${youthfulness}`}
         />
       </div>
-      <div className="flex bg-background rounded-[inherit] rounded-t-none px-4 pt-1.5 pb-2.5 sm:px-5 sm:py-5 justify-between z-10">
-        <div>
-          <h3 className="font-medium text-lg uppercase mb-2 sm:font-semibold text-start">
-            {name}
-          </h3>
-          <dl className="flex gap-2">
-            <dt className="sr-only">Sexo</dt>
-            <dd className="font-medium text-xs capitalize lg:text-base text-[#525252] ">
-              {pet.sexo}
-            </dd>
-            <dt className="sr-only">Edad</dt>
-            <dd className="font-medium text-xs capitalize lg:text-base text-[#525252]">
-              {youthfulness}
-            </dd>
-          </dl>
-        </div>
+      <div className="grid grid-cols-2 bg-background rounded-[inherit] rounded-t-none px-4 pt-2 pb-2.5 sm:px-5 sm:py-5 z-10">
+        <h3 className="font-medium sm:text-lg uppercase sm:font-semibold text-start self-center">
+          {name}
+        </h3>
+        <dl className="flex gap-2 row-start-2">
+          <dt className="sr-only">Sexo</dt>
+          <dd className="font-medium text-xs capitalize lg:text-base text-[#525252] ">
+            {pet.sexo}
+          </dd>
+          <dt className="sr-only">Edad</dt>
+          <dd className="font-medium text-xs capitalize lg:text-base text-[#525252]">
+            {youthfulness}
+          </dd>
+        </dl>
         {shelter && (
           <Image
-            className="shrink-0 self-center lg:size-12 rounded-full"
+            className="shrink-0 lg:size-12 rounded-full justify-self-end"
             src={composeUrl(shelter.logo.url)}
             alt={shelter.logo.alternativeText}
-            height={34}
-            width={34}
+            height={30}
+            width={30}
           />
         )}
       </div>
